16c7d592dc67064e40bf5f5d798b1442f7fdaea9
Alexander Kerner <alexkerner1000@gmail.com>
2017-07-06 10:39:25 +0200

    JavaDoc

--- simple/src/main/java/com/github/drbookings/BookingFactory.java
+++ simple/src/main/java/com/github/drbookings/BookingFactory.java
@@ -4,8 +4,13 @@ import java.util.Collection;
 
 import com.github.drbookings.model.ser.BookingBeanSer;
 
+/**
+ * @version $id$
+ * @author alex
+ *
+ */
 public interface BookingFactory {
 
-    Collection<BookingBeanSer> build() throws Exception;
+	Collection<BookingBeanSer> build() throws Exception;
 
 }
